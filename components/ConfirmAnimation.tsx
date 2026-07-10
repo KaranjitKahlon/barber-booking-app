1272bbf9c982c0186284926d104448f0f7217d0f
Karanjit Kahlon <maxomadedat@gmail.com>
2026-07-10 16:27:10 -0700

    debugged animation issue

--- components/ConfirmAnimation.tsx
+++ components/ConfirmAnimation.tsx
@@ -1,13 +1,14 @@
 "use client"
+import Image from "next/image"
 
 export default function ConfirmAnimation() {
   return (
-    <div className="confirm-container">
-      <div className="confirm-circle">
-        <div style={{color:"white", fontSize:"40px"}}>
-          ✓
-        </div>
-      </div>
-    </div>
+    <Image
+      src="/circle.png"
+      alt="Confirmation Animation"
+      width={100}
+      height={100}
+    className="[margin-top:24px] [animation:popIn_0.5s_ease-out_forwards]">
+    </Image>
   )
 }
