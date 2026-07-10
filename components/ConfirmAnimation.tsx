04f7e8769aadb73d7824cef51af004835e3c3bf7
Karanjit Kahlon <maxomadedat@gmail.com>
2026-07-10 16:07:28 -0700

    testing css animated component

--- components/ConfirmAnimation.tsx
+++ components/ConfirmAnimation.tsx
@@ -2,18 +2,11 @@
 
 export default function ConfirmAnimation() {
   return (
-    <div className="mt-8 flex h-[200px] w-[200px] items-center justify-center">
-      <div className="success-circle">
-        <svg
-          className="check-svg"
-          viewBox="0 0 100 100"
-          fill="none"
-        >
-          <path
-            className="check-path"
-            d="M25 52 L42 68 L75 32"
-          />
-        </svg>
+    <div className="confirm-container">
+      <div className="confirm-circle">
+        <div style={{color:"white", fontSize:"40px"}}>
+          ✓
+        </div>
       </div>
     </div>
   )
