74074ca46676c067294b1f70a04c6a0ae775533e
Karanjit Kahlon <maxomadedat@gmail.com>
2026-07-10 15:14:36 -0700

    updated animation confirm icon

--- components/ConfirmAnimation.tsx
+++ components/ConfirmAnimation.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function ConfirmAnimation() {
     return (
         <Image 
-        src="greenChecklist.svg"
+        src="/Checkmark.gif"
         alt="Confirmation Animation"
         width={200}
         height={200}
