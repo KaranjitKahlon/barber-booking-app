dcc81db6c3ecc667b7b8c10cd24dd1b83c0e2d70
Karanjit Kahlon <maxomadedat@gmail.com>
2026-08-19 19:10:50 -0700

    Changed background color of service cards

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -19,13 +19,13 @@ type ServiceCardProps = {
 
 export default function ServiceCard( { title, description, price, onClick, selectedService } : ServiceCardProps) {
   return (
-    <Card onClick={onClick} className={`[width:250px] [padding:16px] transition-transform hover:[transform:scale(1.05)] cursor-pointer ${selectedService ? "[border:2px_solid_#80ed99] [transform:scale(1.05)]" : ""}`}>
+    <Card onClick={onClick} className={`bg-[#DEE2E6] [width:250px] [min-height:100px] [padding:16px] rounded-sm transition-transform hover:[transform:scale(1.05)] cursor-pointer ${selectedService ? "[border:2px_solid_#80ed99] [transform:scale(1.05)]" : ""}`}>
       <CardHeader className="[padding:24px]">
         <CardTitle>{title}</CardTitle>
         <CardDescription>{description}</CardDescription>
       </CardHeader>
-      <CardFooter className="flex justify-between items-center [padding:24px]">
-        <p className="text-2xl font-bold text-center">${price}</p>
+      <CardFooter className="bg-[#DEE2E6] flex justify-between items-center [padding:24px]">
+        <p className="text-[#111821] text-2xl font-bold text-center">${price}</p>
       </CardFooter>
     </Card>
 
